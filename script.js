44208df7253b4f6ffa2d38fa41ace17f103f7239
Sean Davison <seandavison1998@hotmail.com>
2021-04-27 19:00:35 +0100

    first comit

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const firstName = prompt("What is your name?");
 const lastName = prompt("What is your lastname?");
-const color = prompt("What is your favourite color?");
+const color = prompt("What is your favourite colour?");
 
 let passwordGenerator =
   firstName.slice(0, 1) +
